66717c7c6dfc9b31c09feb60ee04d31d6a150790
Dustin Freeman <dustinfreeman@dustinmacbook.brimstone.dgp.sandbox>
2013-05-02 00:16:25 -0400

    Output new/lost big images

--- ba_data_process/DisplayHandThumbs.py
+++ ba_data_process/DisplayHandThumbs.py
@@ -3,6 +3,32 @@ from PIL import Image
 import numpy as np
 
 #import of bunch of handthumbs and display them.
+MAX_DISPLAY = 250
+META_DIM = 16
+
+
+def make_image(data_path, index_list, image_name):
+	data_file = open (data_path,"rb")
+	data = np.load(data_file)
+	data_file.close()
+
+	big_img = Image.new("F", (META_DIM*IMAGE_DIM,META_DIM*IMAGE_DIM), "black")
+	big_pixels = big_img.load()
+
+	for i in range(len(index_list)):
+		big_x = i % META_DIM
+		big_y = i / META_DIM
+
+		for x in range(IMAGE_DIM):
+			for y in range(IMAGE_DIM):
+				val = data[index_list[i]][x + IMAGE_DIM*y]*255
+
+				big_pixels[big_x*IMAGE_DIM + x, big_y*IMAGE_DIM + y] = val
+
+	big_img = big_img.convert("RGB")
+	big_img.save(image_name)
+
+
 
 def displayThumbs(labels_path,data_path,trained_labels_path):
 	trained_labels_file = open(trained_labels_path,"r")
@@ -11,11 +37,13 @@ def displayThumbs(labels_path,data_path,trained_labels_path):
 	trained_labels = np.load(trained_labels_file)
 	labels = np.load(labels_file)
 	data = np.load(data_file)
+	data_file.close()
+	labels_file.close()
+	trained_labels_file.close()
 
 	hand_indices = []
 	trained_hand_indices = []
 
-	META_DIM = 16
 	big_img = Image.new("F", (META_DIM*IMAGE_DIM,META_DIM*IMAGE_DIM), "black")
 	big_pixels = big_img.load()
 
@@ -46,30 +74,54 @@ def displayThumbs(labels_path,data_path,trained_labels_path):
 			hand_indices.append(i)
 
 		if (trained_labels[i][1] > trained_labels[i][0]):
-			big_trained_x = len(trained_hand_indices) % META_DIM
-			big_trained_y = len(trained_hand_indices) / META_DIM
+			#big_trained_x = len(trained_hand_indices) % META_DIM
+			#big_trained_y = len(trained_hand_indices) / META_DIM
 
 			for x in range(IMAGE_DIM):
 				for y in range(IMAGE_DIM):
 					val = data[i][x + IMAGE_DIM*y]*255
 
-					big_trained_pixels[big_trained_x*IMAGE_DIM + x, big_trained_y*IMAGE_DIM + y] = val
+					#HACK big_trained_pixels[big_trained_x*IMAGE_DIM + x, big_trained_y*IMAGE_DIM + y] = val
 
 			trained_hand_indices.append(i)
 
 	big_img = big_img.convert("RGB")
 	big_img.save("pre-training_hand.png")
 
+	same = 0
+	new = 0
+	new_labels = []
+	lost = 0
+	lost_labels = []
+	for i in range(len(hand_indices)):
+		if hand_indices[i] in trained_hand_indices:
+			same += 1
+		else:
+			lost += 1
+			lost_labels.append(i)
+
+	for i in range(len(trained_hand_indices)):
+		if not trained_hand_indices[i] in hand_indices:
+			new += 1
+			new_labels.append(i)
 
 	big_trained_img = big_trained_img.convert("RGB")
 	big_trained_img.save("training_hand.png")
 
-	print len(hand_indices)
-	print len(trained_hand_indices)
+	print "Original hand labels " + str(len(hand_indices))
+	print "Trained hand labels " + str(len(trained_hand_indices))
+	print "same " + str(same)
+	print "new " + str(new)
+	print "lost " + str(lost)
+
+	make_image(data_path, new_labels, "new_labels.png")
+	make_image(data_path, lost_labels, "lost_labels.png")
 
 if __name__ == "__main__":
-	labels_path = "validation_labels.npy"
-	data_path = "validation.npy"
-	trained_labels_path = "trained/validation_trained.npy"
+	#labels_path = "ba_batchsorted/validation_batchsorted_labels.npy"
+	labels_path = "ba_unsorted/validation_labels.npy"
+	#data_path = "ba_batchsorted/validation_batchsorted.npy"
+	data_path = "ba_unsorted/validation.npy"
+	trained_labels_path = "ba_batchsorted/results/validation_trained.npy"
 
 	displayThumbs(labels_path,data_path,trained_labels_path)
